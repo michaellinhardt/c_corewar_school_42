c1eeb0649a513436850daabde63953f465d00e86
Aodren Bary <abary@student.42.fr>
2016-09-06 18:02:52 +0200

    save

--- instruction/sources/ft_place_chmpions.c
+++ instruction/sources/ft_place_chmpions.c
@@ -8,10 +8,6 @@ void	ft_place_champions(char *vm, t_champion *champions, int champs)
 	nbr = 0;
 	while (nbr < champs)
 	{
-		/*
-		ft_putnbr(nbr % MEM_SIZE);
-		ft_putendl("");
-		*/
 		ft_memmove(vm + ((nbr * MEM_SIZE) / champs), champions->prog_size, CHAMP_MAX_SIZE);
 		++champions;
 		++nbr;
