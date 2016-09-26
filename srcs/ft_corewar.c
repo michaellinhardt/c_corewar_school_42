d803d1abbd2e27ab54060ea3917bc190c0cc31f0
Michael Linhardt <mlinhard@student.42.fr>
2016-09-26 17:29:19 +0200

    ready to implement cor importation

--- srcs/ft_corewar.c
+++ srcs/ft_corewar.c
@@ -21,7 +21,7 @@ int		main(int argc, char **argv)
 	d->vm.graphic = GRAPHIC_MODE;
 	d->vm.console = CONSOLE_LOG;
 
-
+/*
 	wip(d);
 
 	int fd;
@@ -71,77 +71,10 @@ int		main(int argc, char **argv)
 
 		ft_printf("Comment (%lu octet): %s\n", sizeof(file_header.comment), file_header.comment);
 
-
-		/*
-		read( fd, line, 4);
-		ft_printf("READ 4 PREMIER OCTETS (4 octets):\n");
-		ft_printf("\tversion hexa ->\n");
-		ft_printf("\toctets 1: %x\n", line[0]);
-		ft_printf("\toctets 2: %x\n", line[1]);
-		ft_printf("\toctets 3: %x\n", line[2]);
-		ft_printf("\toctets 4: %x\n", line[3]);
-		ft_printf("\n");
-
-		ft_printf("\tversion unsigned char ->\n");
-		ft_printf("\toctets 1: %hhu\n", line[0]);
-		ft_printf("\toctets 2: %hhu\n", line[1]);
-		ft_printf("\toctets 3: %hhu\n", line[2]);
-		ft_printf("\toctets 4: %hhu\n", line[3]);
-
-		ft_printf("--\n");
-
-		ft_printf("NOM DU PROGRAMME (%d octets):\n", PROG_NAME_LENGTH);
-		read (fd, line, PROG_NAME_LENGTH + 1);
-		ft_printf("\t%s\n", line);
-		ft_printf("--\n");
-
-		ft_printf("PROGRAMME SIZE (%d octets):\n", sizeof(unsigned int));
-		read (fd, line, 4);
-		ft_printf("\t%u\n", line);
-		ft_printf("--\n");
-
-
-		ft_printf("COMMENTAIRE (%d octets):\n", COMMENT_LENGTH);
-		read (fd, line, COMMENT_LENGTH + 1);
-		ft_printf("\t");
-		write(1, &line, 2048);
-		ft_printf("\n");
-		ft_printf("--\n");
-
-
-		ft_printf("CODE (%d octets):\n", COMMENT_LENGTH);
-		read (fd, line, COMMENT_LENGTH);
-
-		int empty = -1;
-		while (line[++empty] == 0)
-			;
-		empty++;
-		ft_printf("empty char: %d\n", empty);
-		empty--;
-
-
-		int octet = -1;
-		// while (++octet < 682)
-		while (line[++octet + empty] != 3)
-			ft_printf("[%x]", line[octet + empty]);
-		ft_printf("\n\toctets lus: %d\n", (octet + 1));
-		ft_printf("\tnb caractère hexa: %d\n", (octet + 1) * 2);
-		ft_printf("--\n");
-
-
-
-		ft_printf("\nFIN DE LECTURE SUR: [%x]\n", line[octet + empty]);
-		ft_printf("\tvaleur unsigned char: [%hhu]\n", line[octet + empty]);
-		ft_printf("\tvaleur char: [%c]\n", line[octet + empty]);
-		ft_printf("--\n");
-
-		ft_printf("TAILLE DU FICHIER: %d\n", (octet + 1) + (empty + 1) + 2048 + 128);
-		*/
 	}
 
 	exit (0);
-
-
+*/
 	if (ascii(ASC_LOGO) && ascii(ASC_INIT) && ascii_init()
 		&& ascii(ASC_LOG)
 	&& d->vm.graphic)
