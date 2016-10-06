2799774dcd55b9bd5871a40bf52531e66c4012cb
mlinhard <mlinhard@student.42.fr>
2016-10-06 04:46:36 +0200

    add effect_set() function

--- srcs/ft_mlx_effect_init.c
+++ srcs/ft_mlx_effect_init.c
@@ -3,13 +3,6 @@
 */
 #include "ft_corewar.h"
 
-void		effect_set(t_effect *e, int effect_id, int start_cycle)
-{
-	(void)e;
-	(void)start_cycle;
-	(void)effect_id;
-}
-
 t_effect	*effect_data(void)
 {
 	static t_effect	e[EFFECTS_MAX_SLOT];
@@ -23,11 +16,18 @@ void		effect_init(t_dmlx *m, t_dvm *v, t_effect *e, int i)
 	ft_bzero(e, sizeof(e));
 	data()->mlx.effect = e;
 	e[0].id_max = 1;
+
+	e[0].img = &m->scene_img[2][CPERLOOP_NUMBER];
+	l2(101, "EFFECT_INIT", "scene_img[2][CPERLOOP_NUMBER]", CPERLOOP_NUMBER);
+	l2(101, "EFFECT_INIT", "(looptodie) time to display effect"
+		, (e[0].looptodie_count = 100));
+	l2(101, "EFFECT_INIT", "(inc_value) fade incrementation"
+		, (e[i].inc_value = 255 / e[0].looptodie_count));
+
 	l2(101, "EFFECT_INIT", "(id_max) total of effect", e->id_max);
+
 	while (++i < e->id_max)
 		e[i].id_effect = i;
-	e[0].looptodie = 100;
-	e[0].img = &m->scene_img[2][CPERLOOP_NUMBER];
-	l2(101, "EFFECT_INIT", "(scene_img id) CPERLOOP_NUMBER", CPERLOOP_NUMBER);
+
 	(void)v;
 }
