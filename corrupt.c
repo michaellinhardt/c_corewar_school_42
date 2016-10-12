d59c10a82671faddb18a54478fd8eeade5f5e15a
Aodren Bary <abary@student.42.fr>
2016-10-12 16:09:53 +0200

    test corruption

--- corrupt.c
+++ corrupt.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include "libft.h"
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
 # define COREWAR_EXEC_MAGIC		0xea83f3
@@ -20,6 +21,30 @@ typedef struct		header_s
 	unsigned int		prog_size;
 	char			comment[COMMENT_LENGTH + 1];
 }					header_t;
+
+void	ft_create_code_corrupt(unsigned char *corrupt, unsigned char *code, unsigned int size)
+{
+	int i = 0;
+	int base = 16;
+	
+
+	*corrupt = 0;
+	while (i < size)
+	{
+		if (*code >= 65)
+			*corrupt += (*code - 55) * base;
+		else
+			*corrupt += (*code - 48) * base;
+		base = base == 16 ? 1 : 16;
+		++code;
+		++i;
+		if (!(i % 2) && i < size)
+		{
+			++corrupt;
+			*corrupt = 0;
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -45,19 +70,29 @@ int main(int argc, char **argv)
 	}
 	header.magic =  ((header.magic << 24 & 0xFF000000) | (header.magic << 8 & 0xFF0000)          
 			| (header.magic >> 8 & 0xFF00) | (header.magic >> 24 & 0xFF));
-	header.prog_size =  ((header.prog_size << 24 & 0xFF000000) | (header.prog_size << 8 & 0xFF0000)          
+	int lalalala;
+	lalalala =  ((header.prog_size << 24 & 0xFF000000) | (header.prog_size << 8 & 0xFF0000)          
 			| (header.prog_size >> 8 & 0xFF00) | (header.prog_size >> 24 & 0xFF));
 	if (header.magic != COREWAR_EXEC_MAGIC)
 		printf("heu.., pas le bon magic number fdp\n");
 	int size;
 
+	header.magic =  ((header.magic << 24 & 0xFF000000) | (header.magic << 8 & 0xFF0000)          
+			| (header.magic >> 8 & 0xFF00) | (header.magic >> 24 & 0xFF));
 	size = atoi(*(argv + 2));
-	if (size + strlen(*(argv + 3)) > header.prog_size)
+	if (size + strlen(*(argv + 3)) > lalalala)
 	{
 
 		printf("t'as envie que je segfault???\n");
 		return (0);
 	}	
+
+
+	if (strlen(*(argv + 3)) % 2)
+	{
+		printf("code impair");
+		return (0);
+	}
 	char buf[CHAMP_MAX_SIZE] = {0};
 	char name[13] = {0};
 	if (strlen(*(argv + 1)) > 1024)
@@ -83,10 +118,11 @@ int main(int argc, char **argv)
 	strcpy(header.comment, "WAAAAAAAAAAAAAGGGGGGGGGGGGGGHHHHHHHHHHHHHHHHHHH");
 	char code_corrompu[CHAMP_MAX_SIZE] = {0};
 
-	memcpy(code_corrompu, buf, header.prog_size );
-	memcpy(code_corrompu + size, *(argv + 3), strlen(*(argv + 3)));
+	memcpy(code_corrompu, buf, lalalala );
+//	memcpy(code_corrompu + size, *(argv + 3), strlen(*(argv + 3)));
+	ft_create_code_corrupt((unsigned char *)(code_corrompu + size), (unsigned char *)*(argv + 3), strlen(*(argv + 3)));
 	write(fd, &header, sizeof(header_t));
-	write(fd, code_corrompu, header.prog_size);
+	write(fd, code_corrompu, lalalala);
 	
 	close (fd);
 	return (0);
