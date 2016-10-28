5c7c2e3f775dbffa255d72fa01180e4803f2918b
mlinhard <mlinhard@student.42.fr>
2016-10-28 16:05:21 +0200

    add aura

--- srcs/ft_mlx_display_processus.c
+++ srcs/ft_mlx_display_processus.c
@@ -1,25 +1,5 @@
 #include "ft_corewar.h"
 
-void	put_proc_img(t_img *img, t_img *proc, int x, int y)
-{
-	int			*pproc;
-	int			*pimg;
-
-	pproc = (int *)proc->str;
-	pimg = (int *)img->str;
-	img->i = (y * WIN_X + x) - 1;
-	y = -1;
-	while (++y < PROCY)
-	{
-		x = -1;
-		while (++x < PROCX)
-		{
-			pimg[++(img->i)] = pproc[y * PROCX + x];
-		}
-		img->i = img->i - x + WIN_X;
-	}
-}
-
 void	put_proc_bloc(t_img *img, t_img *bloc, int x, int y)
 {
 	// int			*pbloc;
@@ -44,7 +24,7 @@ void	put_proc_bloc(t_img *img, t_img *bloc, int x, int y)
 
 void	display_bloc(t_dmlx *m, t_proc *proc, t_img *img, t_img *bloc)
 {
-
+	return ;
 	ft_printf("proc_id %d ¶ wait: %d, turfu: %d\n", proc->id, proc->wait, proc->pc_turfu);
 	if (proc->inst)
 		ft_printf("%s\n", proc->inst->name);
@@ -70,7 +50,7 @@ void	display_processus(t_dmlx *m, t_proc *proc, t_img *img, t_img *bloc)
 		}
 		x = (proc->pc % VMPERLINE) * VMSPACEBLANK + VMSTARTX + PROCDECALLAGEX;
 		y = (proc->pc / VMPERLINE) * VMSPACELINE + VMSTARTY + PROCDECALLAGEY;
-		// put_proc_img(img, &m->scene_img[2][-proc->player + 5], x, y);
+		// put_proc_bloc(img, &m->scene_img[2][-proc->player + 5], x, y);
 		x -= 1;
 		y -= 2;
 		if (!proc->inst)
@@ -78,7 +58,7 @@ void	display_processus(t_dmlx *m, t_proc *proc, t_img *img, t_img *bloc)
 		else
 			put_proc_bloc(img, &m->scene_img[2][-proc->player + 27], x, y);
 		already[(proc->pc)] = 1;
-		display_bloc(m, proc, img, bloc);
+		// display_bloc(m, proc, img, bloc);
 		proc = proc->n;
 	}
 }
