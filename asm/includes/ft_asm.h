4da51c5418e97c52d7164114166ee78ac734de53
Aodren Bary <abary@student.42.fr>
2016-11-01 19:04:31 +0100

    Mise a la norme des .h

--- asm/includes/ft_asm.h
+++ asm/includes/ft_asm.h
@@ -1,153 +1,155 @@
 #ifndef FT_ASM_H
 # define FT_ASM_H
-
-#include "ft_struct_asm.h"
+# include "ft_struct_asm.h"
 # define NBR_INST 17
 
 /*
 ** Recuperation des infos du (des) fichiers
 */
-int			ft_open_file(char **argv);
-int			ft_get_size_file(int fd);
-char		*ft_get_line(int fd, t_lexer *lexer);
+int				ft_open_file(char **argv);
+int				ft_get_size_file(int fd);
+char			*ft_get_line(int fd, t_lexer *lexer);
+int				ft_little_to_big(int little);
+void			ft_destruct_lexer(t_lexer *lexer);
+void			ft_destruct_parser(t_parser *parser);
 
- 
-int			ft_little_to_big(int little);
-
-void		ft_destruct_lexer(t_lexer *lexer);
-void		ft_destruct_parser(t_parser *parser);
 /*
 ** Instructions
 */
 
-void		ft_init_instructions(t_instructions *inst);
-void		ft_init_instructions_name(t_instructions *inst);
-void		ft_init_instructions_nbr_args(t_instructions *inst);
-void		ft_init_instructions_types_arg_zero(t_instructions *inst);
-void		ft_init_instructions_types_arg_un(t_instructions *inst);
-void		ft_init_instructions_types_arg_deux(t_instructions *inst);
+void			ft_init_instructions(t_instructions *inst);
+void			ft_init_instructions_name(t_instructions *inst);
+void			ft_init_instructions_nbr_args(t_instructions *inst);
+void			ft_init_instructions_types_arg_zero(t_instructions *inst);
+void			ft_init_instructions_types_arg_un(t_instructions *inst);
+void			ft_init_instructions_types_arg_deux(t_instructions *inst);
+
 /*
 ** Lexer
 */
-t_token		*ft_new_token(t_lexer *lexer);
-void		ft_init_lexer(t_lexer *lexer);
-int			ft_lexer(t_lexer *lexer, int i, int ret);
-void		ft_get_value(t_lexer *lexer, t_token *token, int size);
 
-
-void		ft_free_tokens(t_token *token);
-int			ft_lexer_error(t_lexer *lexer, int erreur);
-void		ft_free_elem_pile(t_pile_tree *tree, t_parser *parser);
-
-/*
-**	Display
-*/
-void		ft_display_tokenisation(t_token *token);
-void		ft_display_type_token(int token);
-void		ft_display_parse_tree(t_parse_tree *tree);
+t_token			*ft_new_token(t_lexer *lexer);
+void			ft_init_lexer(t_lexer *lexer);
+int				ft_lexer(t_lexer *lexer, int i, int ret);
+void			ft_get_value(t_lexer *lexer, t_token *token, int size);
+void			ft_free_tokens(t_token *token);
+int				ft_lexer_error(t_lexer *lexer, int erreur);
+void			ft_free_elem_pile(t_pile_tree *tree, t_parser *parser);
 
 /*
-**			Tokenisation
+**		Display
 */
 
-int			ft_lexer_command_name(t_lexer *lexer, t_token *token);
-int			ft_lexer_command_comment(t_lexer *lexer, t_token *token);
-int			ft_lexer_comment(t_lexer *lexer, t_token *token);
-int			ft_lexer_string(t_lexer *lexer, t_token *token);
-int			ft_lexer_whitespace(t_lexer *lexer, t_token *token);
-int			ft_lexer_separator(t_lexer *lexer, t_token *token);
-int			ft_lexer_label(t_lexer *lexer, t_token *token);
-int			ft_lexer_direct(t_lexer *lexer, t_token *token);
-int			ft_lexer_instruction(t_lexer *lexer, t_token *token);
-int			ft_lexer_indirect(t_lexer *lexer, t_token *token);
-int			ft_lexer_indirect_label(t_lexer *lexer, t_token *token);
-int			ft_lexer_direct_label(t_lexer *lexer, t_token *token);
-int			ft_lexer_register(t_lexer *lexer, t_token *token);
-int			ft_lexer_endline(t_lexer *lexer, t_token *token);
-int			ft_lexer_end(t_lexer *lexer, t_token *token);
+void			ft_display_tokenisation(t_token *token);
+void			ft_display_type_token(int token);
+void			ft_display_parse_tree(t_parse_tree *tree);
 
 /*
-**	Parser
+**				Tokenisation
 */
 
-int			ft_parser(t_token *token, t_parser *parser);
-int			ft_parse_error(t_parser *parser, int erreur , t_token *token);
-void		ft_init_parser(t_parser *parser);
+int				ft_lexer_command_name(t_lexer *lexer, t_token *token);
+int				ft_lexer_command_comment(t_lexer *lexer, t_token *token);
+int				ft_lexer_comment(t_lexer *lexer, t_token *token);
+int				ft_lexer_string(t_lexer *lexer, t_token *token);
+int				ft_lexer_whitespace(t_lexer *lexer, t_token *token);
+int				ft_lexer_separator(t_lexer *lexer, t_token *token);
+int				ft_lexer_label(t_lexer *lexer, t_token *token);
+int				ft_lexer_direct(t_lexer *lexer, t_token *token);
+int				ft_lexer_instruction(t_lexer *lexer, t_token *token);
+int				ft_lexer_indirect(t_lexer *lexer, t_token *token);
+int				ft_lexer_indirect_label(t_lexer *lexer, t_token *token);
+int				ft_lexer_direct_label(t_lexer *lexer, t_token *token);
+int				ft_lexer_register(t_lexer *lexer, t_token *token);
+int				ft_lexer_endline(t_lexer *lexer, t_token *token);
+int				ft_lexer_end(t_lexer *lexer, t_token *token);
 
-int			ft_parser_shift(t_parser *parser);
-int			ft_parser_reduce(t_parser *parser);
-int			ft_parser_accept(t_parser *parser);
+/*
+**		Parser
+*/
 
-void		ft_free_pointeur_tab_fils(t_parse_tree *tree);
-void		ft_free_arguments(t_parse_tree *tree);
+int				ft_parser(t_token *token, t_parser *parser);
+int				ft_parse_error(t_parser *parser, int erreur, t_token *token);
+void			ft_init_parser(t_parser *parser);
+
+int				ft_parser_shift(t_parser *parser);
+int				ft_parser_reduce(t_parser *parser);
+int				ft_parser_accept(t_parser *parser);
+
+void			ft_free_pointeur_tab_fils(t_parse_tree *tree);
+void			ft_free_arguments(t_parse_tree *tree);
 t_parse_tree	*ft_fusion_fils(t_parse_tree *t1, t_parse_tree *t2);
-/*
-**			rules
-*/
-int			ft_rule_name(t_parser *parser, t_pile_tree *pile);
-int			ft_rule_ccomment(t_parser *parser, t_pile_tree *pile);
-int			ft_rule_endline(t_parser *parser, t_pile_tree *pile);
-int			ft_rule_instruction(t_parser *parser, t_pile_tree *pile);
-int			ft_rule_separator_char(t_parser *parser, t_pile_tree *pile);
-int			ft_rule_argument(t_parser *parser, t_pile_tree *tree);
-int			ft_rule_end(t_parser *parser, t_pile_tree *tree);
-int			ft_rule_label(t_parser *parser, t_pile_tree *tree);
 
 /*
-**			accept rules
+**				rules
 */
 
-int			ft_accept_header(t_parser *parser, t_pile_tree *pile);
-int			ft_accept_name(t_parser *parser, t_pile_tree *pile);
-int			ft_accept_comment(t_parser *parser, t_pile_tree *pile);
-int			ft_accept_argument(t_parser *parser, t_pile_tree *pile);
-int			ft_accept_instruction(t_parser *parser, t_pile_tree *pile);
-int			ft_accept_instruction_plus(t_parser *parser, t_pile_tree *pile);
-int			ft_accept_separator_char(t_parser *parser, t_pile_tree *tree);
-int			ft_accept_label_inst(t_parser *parser, t_pile_tree *tree);
-int			ft_accept_position_inst(t_parser *parser, t_pile_tree *tree);
-int			ft_accept_fin(t_parser *parser, t_pile_tree *tree);
-int			ft_accept_code_fin(t_parser *parser, t_pile_tree *tree);
+int				ft_rule_name(t_parser *parser, t_pile_tree *pile);
+int				ft_rule_ccomment(t_parser *parser, t_pile_tree *pile);
+int				ft_rule_endline(t_parser *parser, t_pile_tree *pile);
+int				ft_rule_instruction(t_parser *parser, t_pile_tree *pile);
+int				ft_rule_separator_char(t_parser *parser, t_pile_tree *pile);
+int				ft_rule_argument(t_parser *parser, t_pile_tree *tree);
+int				ft_rule_end(t_parser *parser, t_pile_tree *tree);
+int				ft_rule_label(t_parser *parser, t_pile_tree *tree);
+
 /*
-**			checks
+**				accept rules
 */
 
-int			ft_check_name_instructions(char *instructions, t_instructions *inst);
-void		ft_add_leaf(t_parse_tree *tree, t_parse_tree *leaf);
-void		ft_add_tree_shift(t_parser *parser);
-void		ft_add_parent_tree(t_pile_tree *tree, t_parser *parser);
-int			ft_verif_instruction(t_pile_tree *pile, t_pile_tree *next,
+int				ft_accept_header(t_parser *parser, t_pile_tree *pile);
+int				ft_accept_name(t_parser *parser, t_pile_tree *pile);
+int				ft_accept_comment(t_parser *parser, t_pile_tree *pile);
+int				ft_accept_argument(t_parser *parser, t_pile_tree *pile);
+int				ft_accept_instruction(t_parser *parser, t_pile_tree *pile);
+int				ft_accept_instruction_plus(t_parser *parser, t_pile_tree *pile);
+int				ft_accept_separator_char(t_parser *parser, t_pile_tree *tree);
+int				ft_accept_label_inst(t_parser *parser, t_pile_tree *tree);
+int				ft_accept_position_inst(t_parser *parser, t_pile_tree *tree);
+int				ft_accept_fin(t_parser *parser, t_pile_tree *tree);
+int				ft_accept_code_fin(t_parser *parser, t_pile_tree *tree);
+
+/*
+**				checks
+*/
+
+int				ft_check_name_instructions(char *instructions,
 		t_instructions *inst);
+void			ft_add_leaf(t_parse_tree *tree, t_parse_tree *leaf);
+void			ft_add_tree_shift(t_parser *parser);
+void			ft_add_parent_tree(t_pile_tree *tree, t_parser *parser);
+int				ft_verif_instruction(t_pile_tree *pile, t_pile_tree *next,
+			t_instructions *inst);
 
 /*
-**			shifts
+**				shifts
 */
 
-int			ft_shift_no_focus(t_parser *parser);
-int			ft_shift_whitespaces(t_parser *parser);
-int			ft_shift_command_name(t_parser *parser);
-int			ft_shift_string(t_parser *parser);
-int			ft_shift_endline(t_parser *parser);
-int			ft_shift_command_comment(t_parser *parser);
-int			ft_shift_label(t_parser *parser);
-int			ft_shift_instruction(t_parser *parser);
-int			ft_shift_register(t_parser *parser);
-int			ft_shift_separator_char(t_parser *pasrer);
-int			ft_shift_direct_label(t_parser *pasrer);
-int			ft_shift_direct(t_parser *pasrer);
-int			ft_shift_indirect(t_parser *parser);
-int			ft_shift_indirect_label(t_parser *parser);
-int			ft_shift_comment(t_parser *parser);
-int			ft_shift_end(t_parser *parser);
+int				ft_shift_no_focus(t_parser *parser);
+int				ft_shift_whitespaces(t_parser *parser);
+int				ft_shift_command_name(t_parser *parser);
+int				ft_shift_string(t_parser *parser);
+int				ft_shift_endline(t_parser *parser);
+int				ft_shift_command_comment(t_parser *parser);
+int				ft_shift_label(t_parser *parser);
+int				ft_shift_instruction(t_parser *parser);
+int				ft_shift_register(t_parser *parser);
+int				ft_shift_separator_char(t_parser *pasrer);
+int				ft_shift_direct_label(t_parser *pasrer);
+int				ft_shift_direct(t_parser *pasrer);
+int				ft_shift_indirect(t_parser *parser);
+int				ft_shift_indirect_label(t_parser *parser);
+int				ft_shift_comment(t_parser *parser);
+int				ft_shift_end(t_parser *parser);
 /*
 ** Parse tree
 */
 
-void		ft_add_node(t_parse_tree *tree, t_parse_tree *leaf);
-t_parse_tree *ft_create_leaf(t_token *token);
+void			ft_add_node(t_parse_tree *tree, t_parse_tree *leaf);
+t_parse_tree	*ft_create_leaf(t_token *token);
 
 /*
-**	compilation
+**		compilation
 */
 
 int				ft_create_file(char *name);
@@ -155,19 +157,20 @@ int				ft_calcul_poids(t_parse_tree *tree, t_instructions *inst,
 				unsigned int poids);
 void			ft_init_compilation(t_compile *compile);
 int				ft_compilation(t_parse_tree *tree, t_parser *parser,
-				t_lexer *lexer);
+					t_lexer *lexer);
 unsigned char	ft_set_oc_p(unsigned int id, t_parse_tree *inst);
-int				ft_compile(t_parse_tree *tree, unsigned char **code, 
-				t_compile *compile, unsigned int size);
+int				ft_compile(t_parse_tree *tree, unsigned char **code,
+					t_compile *compile, unsigned int size);
 int				ft_search_label(t_parse_tree *root, t_parse_tree *tree);
 char			*ft_create_name(char *name, int size);
 int				ft_compile_register(t_parse_tree *tree, unsigned char *code);
-int				ft_compile_direct(t_parse_tree *tree, unsigned char *code,
-		int flag_dir);
-int				ft_compile_direct_label(t_parse_tree *tree, unsigned char *code,
-		int flag_dir);
-int				ft_compile_indirect(t_parse_tree *tree, unsigned char *code);
-int				ft_compile_instruction(t_parse_tree *tree, unsigned char **code,
-		const t_instructions *inst, unsigned int size);
+int				ft_compile_direct(t_parse_tree *tree,
+		unsigned char *code, int flag_dir);
+int				ft_compile_direct_label(t_parse_tree *tree,
+		unsigned char *code, int flag_dir);
+int				ft_compile_indirect(t_parse_tree *tree,
+		unsigned char *code);
+int				ft_compile_instruction(t_parse_tree *tree,
+		unsigned char **code, const t_instructions *inst, unsigned int size);
 
 #endif
--- asm/includes/ft_struct_asm.h
+++ asm/includes/ft_struct_asm.h
@@ -1,24 +1,21 @@
 #ifndef FT_STRUCT_ASM_H
 # define FT_STRUCT_ASM_H
 # include "op.h"
-
 # define NBR_REDUCE 8
 # define NBR_SHIFT 16
-
-
-#define LEXER_STRING -1
-#define LEXER_INCONNU -2
-
-#define PARSER_INSTRUCTION_WRONG -1
-#define PARSER_INSTRUCTION_NBR -2
-#define PARSER_INSTRUCTION_REGISTRE -3
-#define PARSER_ARG_WRONG -4
-
+# define LEXER_STRING -1
+# define LEXER_INCONNU -2
+# define PARSER_INSTRUCTION_WRONG -1
+# define PARSER_INSTRUCTION_NBR -2
+# define NBR_ACCEPT 11
+# define PARSER_INSTRUCTION_REGISTRE -3
+# define PARSER_ARG_WRONG -4
 # define RED "\033[0;31m"
 # define NEUTRE "\033[0;m"
-enum	e_token{
+
+enum				e_token{
 	NONE,
-	COMMAND_NAME, 
+	COMMAND_NAME,
 	COMMAND_COMMENT,
 	COMMENT,
 	STRING,
@@ -34,34 +31,50 @@ enum	e_token{
 	ENDLINE,
 	END,
 	NBR_TOKEN
-};	
-
-
-typedef struct s_pile_tree t_pile_tree;
-typedef struct s_instructions t_instructions;
-typedef struct s_token t_token;
-typedef struct s_lexer t_lexer;
-typedef struct s_parser t_parser;
-typedef struct s_parse_tree t_parse_tree;
-typedef struct s_compile t_compile;
-/*
-typedef unsigned char * (*f_compilation[ENDLINE])(t_parse_tree *tree, unsigned 
-		char *code, t_compile *compile);
-		*/
-struct          s_instructions
-{
-	int						id;
-	char					name[6];
-	short					nbr_args;
-	t_arg_type				types[MAX_ARGS_NUMBER];
-	int						flag_ocp;
-	int						flag_size_ind;
 };
 
+enum				e_value	{NO_VALUE,
+	MINI_NAME,
+	CMD_NAME,
+	MINI_COMMENT,
+	CMD_COMMENT,
+	HEADER,
+	ARG,
+	INST,
+	FIN_LINE,
+	LAST_ARG,
+	CPL_INST,
+	POSITION,
+	POSITION_INST,
+	VIRGULE,
+	CODE_FIN,
+	FIN};
 
+enum				e_action {ERREUR,
+	SHIFT,
+	REDUCE,
+	ACCEPT,
+	CODE_ACCEPT};
 
+typedef struct s_pile_tree		t_pile_tree;
+typedef struct s_instructions	t_instructions;
+typedef struct s_token			t_token;
+typedef struct s_lexer			t_lexer;
+typedef struct s_parser			t_parser;
+typedef struct s_parse_tree		t_parse_tree;
+typedef struct s_compile		t_compile;
 
-struct	s_compile
+struct				s_instructions
+{
+	int				id;
+	char			name[6];
+	short			nbr_args;
+	t_arg_type		types[MAX_ARGS_NUMBER];
+	int				flag_ocp;
+	int				flag_size_ind;
+};
+
+struct				s_compile
 {
 	t_header		header;
 	unsigned char	*code;
@@ -69,7 +82,7 @@ struct	s_compile
 	unsigned int	total_size;
 	t_instructions	inst[17];
 	t_instructions	*actual_inst;
-	int				flag_size_ind; // c'est pas direct ?
+	int				flag_size_ind;
 	unsigned char	*(*f_compile[ENDLINE + 1])(t_parse_tree *tree, unsigned char
 			*code, t_compile *compile);
 };
@@ -88,70 +101,47 @@ struct				s_lexer
 {
 	t_token			*begin;
 	t_token			*end;
-	int				head; // l'endroir tou on est dans le fichier
-	int				offset; // le decalage dans la fonction
+	int				head;
+	int				offset;
 	int				size_line;
 	unsigned int	y;
-	unsigned int 	x;
+	unsigned int	x;
 	char			*line;
 	char			*focus;
 	char			*name;
 	int				size;
-	int				fd; // fd du fichier
+	int				fd;
 	int				(*f_tokens[NBR_TOKEN])(t_lexer *lexer, t_token *token);
 };
 
-// le truc final
-//
 struct				s_parse_tree
 {
 	t_token			*token;
 	int				nbr_fils;
 	int				id_instruction;
-	int				poids; // le poids du bordel
+	int				poids;
 	int				position;
 	t_parse_tree	**fils;
-	// le pointeur sur fonction pour l'action a effectuer
 };
-
-
-/*
-enum	e_value {NO_VALUE, MINI_NAME, CMD_NAME, MINI_COMMENT, CMD_COMMENT,
-				HEADER,
-				INST, ARG, VIRGULE, LAST_ARG, CPL_INST, FIN_INST, POSITION,
-				FIN_LINE, FIN_LABEL,
-				FIN};
-				*/
-# define NBR_ACCEPT 11
-enum	e_value	{NO_VALUE, MINI_NAME, CMD_NAME, MINI_COMMENT, CMD_COMMENT,
-	HEADER, ARG, INST, FIN_LINE, LAST_ARG, CPL_INST, POSITION, POSITION_INST,
-	VIRGULE, CODE_FIN, FIN};
-
-enum	e_action {ERREUR, SHIFT, REDUCE, ACCEPT, CODE_ACCEPT};
 
 struct				s_pile_tree
 {
 	int				value;
-//	t_instructions	*inst;
 	t_parse_tree	*tree;
 	t_pile_tree		*next;
 	t_pile_tree		*prev;
 };
 
-
-typedef char            t_arg_type;
-typedef struct s_dvm    t_dvm;
-typedef struct s_proc   t_proc;
-
-struct					s_parser
+struct				s_parser
 {
 	int				size;
 	int				size_fonction;
-	int				(*f_reduce[NBR_REDUCE])(t_parser *parser, t_pile_tree *pile);
+	int				(*f_reduce[NBR_REDUCE])(t_parser *parser,
+			t_pile_tree *pile);
 	int				(*f_shift[NBR_SHIFT])(t_parser *parser);
-	int				(*f_accept[NBR_ACCEPT])(t_parser *parser, t_pile_tree *tree);
+	int				(*f_accept[NBR_ACCEPT])(t_parser *parser,
+			t_pile_tree *tree);
 	t_instructions	inst[17];
-//	t_memory		memory;
 	t_token			*focus;
 	t_pile_tree		*debut_pile;
 	t_pile_tree		*end_pile;
