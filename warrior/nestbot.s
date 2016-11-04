4e7a5b5e8015a21f543825bc2d8362ea6b29f7f7
Michael Linhardt <mlinhard@student.42.fr>
2016-11-04 19:02:53 +0100

    save champ

--- warrior/nestbot.s
+++ warrior/nestbot.s
@@ -1,15 +1,58 @@
-#by zaz, still not as good as rainbowdash though
-#2013
-
 .name		"nestbot"
-.comment	"pick a cookie !"
+.comment	"pick a cookie !!"
 
+# ecris R1 dans param de live1
+prep_live:
+	sti		r1, %:live1, %1
+
+# ecrase R1 avec les param de live1
+erase_reg:
+	ldi		%:live1, %1, r1
+
+# ecris R1 dans param de live1
+live_countdown:
+	sti		r1, %:live2, %1
+
+# remplace le deuxieme LD de live_code par R1
+prep_live_code:
+	sti		r1, %:live_code, %9
+
+# embarque le code dun live pour mon champion
 live_code:
 	ld		%1, r8
+	ld		%0, r9
 
-write_live:
-#write r6 to live_1
-	sti		r6, %:live_1, %1
-
-live_1:
+# execute un live
+live1:
 	live	%255
+
+# ecris un live a la place du prep_live pour proteger la reecriture
+# du live
+erase_prep:
+	sti		r8, %:prep_live, %-1
+	sti		r9, %:prep_live, %3
+
+# boucle de reecriture
+countdown_init:
+	ld		%1, r7
+
+#While (r7 < 255)
+countdown_inc:
+	add		r7, r8, r7
+
+countdown_break:
+	xor		r7, %255, r10
+
+live2:
+	live	%255
+
+countdown_end:
+	zjmp	%:countdown_over
+
+countdown_restart:
+	fork	%:prep_live
+	ld		%0, r10
+	zjmp	%:countdown_inc
+
+countdown_over:
+	zjmp	%:prep_live
